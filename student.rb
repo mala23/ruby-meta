4e198fa8b0b0e76e823cf09dd1810bf5fdac8789
mala23 <mail@mala23.me>
2014-11-17 11:06:00 +0000

    rewrite method_missing

--- student.rb
+++ student.rb
@@ -16,9 +16,16 @@ class Student
     self.class.send(:define_method, new_method){}
   end
 
+  def method_missing(method_name)
+    "It's alright, #{method_name} does not exist, but I've got your back!"
+  end
+
 end
 
 student = Student.new
 # p student
-student.class.send(:define_method, :does_it_return_true?){true}
-p student.does_it_return_true?
+
+# student.class.send(:define_method, :does_it_return_true?){true}
+# p student.does_it_return_true?
+
+p student.missing_method
