22478eb1941453d74e007daddd4461bbaae090a1
mala23 <mail@mala23.me>
2014-11-17 10:56:17 +0000

    defining a method on-the-fly

--- student.rb
+++ student.rb
@@ -8,9 +8,17 @@ class Student
     @badges << badge
   end
 
+  def what_class
+    p self.class
+  end
+
+  def add_method(new_method)
+    self.class.send(:define_method, new_method){}
+  end
 
 end
 
 student = Student.new
 # p student
-p student.class
+student.class.send(:define_method, :does_it_return_true?){true}
+p student.does_it_return_true?
